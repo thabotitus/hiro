306b055b1eb250788590ed0f1a05158d4e14c8e7
Thabo <thabotitus@gmail.com>
2021-11-27 17:03:46 +0200

    dark mode toggle

--- src/js/controllers/hello_controller.js
+++ src/js/controllers/hello_controller.js
@@ -1,7 +1,42 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["body", "colourmodeCheckbox"];
+
+  COLOURMODE_KEY  = 'tts-colourmode';
+  DARK_MODE_CLASS = 'tts-dark-mode';
+
+  GET_LS_STATE = (key) => localStorage.getItem(key);
+  SET_LS_STATE = (key, value) => localStorage.setItem(key, value);
+
   connect() {
-    console.log("Hello, Stimulus is working!");
+    this.setInitialColorMode();
+  }
+
+  setInitialColorMode() {
+    if (this.GET_LS_STATE(this.COLOURMODE_KEY) === 'dark') {
+      this.bodyTarget.classList.add(this.DARK_MODE_CLASS);
+      this.colourmodeCheckboxTarget.checked = true;
+    }
+  }
+
+  toggleDarkMode(evt) {
+    switch (evt.target.checked) {
+      case true:
+        this.bodyTarget.classList.add(this.DARK_MODE_CLASS);
+        this.SET_LS_STATE(this.COLOURMODE_KEY, 'dark');
+        break;
+      default:
+        this.bodyTarget.classList.remove(this.DARK_MODE_CLASS);
+        this.SET_LS_STATE(this.COLOURMODE_KEY, null)
+        break;
+    }
+    // if (evt.target.checked) {
+    //   this.bodyTarget.classList.add(this.DARK_MODE_CLASS);
+    //   this.SET_LS_STATE(this.COLOURMODE_KEY, 'dark');
+    // } else {
+    //   this.bodyTarget.classList.remove(this.DARK_MODE_CLASS);
+    //   this.SET_LS_STATE(this.COLOURMODE_KEY, null);
+    // }
   }
 }
