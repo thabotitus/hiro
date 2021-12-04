b809e107fb979fe04289461bc04dd8a8f016bd60
Thabo <thabotitus@gmail.com>
2021-12-04 17:12:16 +0200

    tables and basic card

--- docs/data/table.js
+++ docs/data/table.js
@@ -53,220 +53,220 @@ export const tableData = [
 		"currency": "$53.53",
 		"country": "Sweden"
 	},
-	{
-		"name": "Samantha Harper",
-		"phone": "(373) 884-7465",
-		"email": "cras@icloud.com",
-		"address": "145-2621 Tincidunt Road",
-		"postalZip": "U6P 1B3",
-		"currency": "$64.52",
-		"country": "Spain"
-	},
-	{
-		"name": "Selma Washington",
-		"phone": "(468) 605-4869",
-		"email": "ac.sem@yahoo.org",
-		"address": "1155 Neque Av.",
-		"postalZip": "1933",
-		"currency": "$38.00",
-		"country": "Mexico"
-	},
-	{
-		"name": "Hanna Shaffer",
-		"phone": "(309) 651-2229",
-		"email": "purus.sapien.gravida@hotmail.com",
-		"address": "821-5665 Donec Av.",
-		"postalZip": "724774",
-		"currency": "$52.68",
-		"country": "Netherlands"
-	},
-	{
-		"name": "Stewart Finley",
-		"phone": "1-752-721-7198",
-		"email": "vel.faucibus.id@protonmail.ca",
-		"address": "Ap #825-7133 Sed Ave",
-		"postalZip": "2588",
-		"currency": "$84.05",
-		"country": "Colombia"
-	},
-	{
-		"name": "Gretchen Logan",
-		"phone": "(669) 298-4624",
-		"email": "aenean.eget.metus@aol.org",
-		"address": "1439 Risus Ave",
-		"postalZip": "96734",
-		"currency": "$43.91",
-		"country": "Vietnam"
-	},
-	{
-		"name": "Jasmine Espinoza",
-		"phone": "1-445-947-6981",
-		"email": "malesuada.malesuada.integer@icloud.couk",
-		"address": "7018 Egestas Street",
-		"postalZip": "27774",
-		"currency": "$62.75",
-		"country": "Costa Rica"
-	},
-	{
-		"name": "Mona Nash",
-		"phone": "(578) 253-9930",
-		"email": "nec.metus@hotmail.net",
-		"address": "P.O. Box 684, 6990 Odio, Street",
-		"postalZip": "6764",
-		"currency": "$39.14",
-		"country": "United States"
-	},
-	{
-		"name": "Roth Burton",
-		"phone": "(995) 628-7064",
-		"email": "sollicitudin.a.malesuada@protonmail.com",
-		"address": "887-6938 Dolor St.",
-		"postalZip": "101674",
-		"currency": "$71.15",
-		"country": "Ireland"
-	},
-	{
-		"name": "Hiroko Newton",
-		"phone": "(263) 227-6818",
-		"email": "sed.pede@yahoo.edu",
-		"address": "242-4371 Vehicula St.",
-		"postalZip": "4427",
-		"currency": "$65.40",
-		"country": "Canada"
-	},
-	{
-		"name": "Rogan Stanley",
-		"phone": "1-982-267-9189",
-		"email": "sagittis.augue.eu@icloud.ca",
-		"address": "692-6399 Eu, Rd.",
-		"postalZip": "444442",
-		"currency": "$83.36",
-		"country": "Mexico"
-	},
-	{
-		"name": "Neil Wall",
-		"phone": "(543) 729-4444",
-		"email": "turpis.nulla@aol.com",
-		"address": "Ap #973-4005 Vitae Ave",
-		"postalZip": "22425-33204",
-		"currency": "$19.14",
-		"country": "Brazil"
-	},
-	{
-		"name": "Tanner Cervantes",
-		"phone": "(366) 366-5859",
-		"email": "vel.arcu@yahoo.net",
-		"address": "440-6026 Vel Road",
-		"postalZip": "488404",
-		"currency": "$62.36",
-		"country": "Belgium"
-	},
-	{
-		"name": "Merrill Avila",
-		"phone": "1-324-588-3211",
-		"email": "dolor.tempus@protonmail.couk",
-		"address": "135-3622 Libero Ave",
-		"postalZip": "3910",
-		"currency": "$43.99",
-		"country": "United States"
-	},
-	{
-		"name": "Alexandra Russo",
-		"phone": "(715) 792-2773",
-		"email": "interdum.enim@google.org",
-		"address": "P.O. Box 954, 1592 Vitae Rd.",
-		"postalZip": "55723",
-		"currency": "$74.02",
-		"country": "Indonesia"
-	},
-	{
-		"name": "Tashya Woodward",
-		"phone": "1-991-466-8566",
-		"email": "convallis.dolor.quisque@google.edu",
-		"address": "5133 Vitae Av.",
-		"postalZip": "3823",
-		"currency": "$31.12",
-		"country": "Sweden"
-	},
-	{
-		"name": "Merrill Martin",
-		"phone": "1-366-403-5784",
-		"email": "faucibus.leo@yahoo.com",
-		"address": "832-3695 Non Rd.",
-		"postalZip": "K4H 2PN",
-		"currency": "$91.26",
-		"country": "Australia"
-	},
-	{
-		"name": "Jeanette Haney",
-		"phone": "(817) 310-8903",
-		"email": "nec.metus.facilisis@yahoo.edu",
-		"address": "638-5626 Lorem Rd.",
-		"postalZip": "35149",
-		"currency": "$85.39",
-		"country": "Sweden"
-	},
-	{
-		"name": "Aristotle Gray",
-		"phone": "(274) 377-1991",
-		"email": "augue@hotmail.net",
-		"address": "Ap #711-7355 Velit. Av.",
-		"postalZip": "41205",
-		"currency": "$11.14",
-		"country": "China"
-	},
-	{
-		"name": "Jane Stevens",
-		"phone": "(458) 774-6233",
-		"email": "ultrices@hotmail.net",
-		"address": "303 Nunc Rd.",
-		"postalZip": "51539",
-		"currency": "$87.29",
-		"country": "Germany"
-	},
-	{
-		"name": "Castor Gibson",
-		"phone": "(645) 385-4416",
-		"email": "quisque.ornare@hotmail.ca",
-		"address": "220-7949 Nunc Rd.",
-		"postalZip": "2822",
-		"currency": "$43.45",
-		"country": "Spain"
-	},
-	{
-		"name": "Melyssa Castillo",
-		"phone": "(286) 146-6744",
-		"email": "quam@google.ca",
-		"address": "Ap #971-1948 Montes, Road",
-		"postalZip": "122738",
-		"currency": "$50.50",
-		"country": "Costa Rica"
-	},
-	{
-		"name": "Julie Ingram",
-		"phone": "(472) 323-6759",
-		"email": "amet.massa@yahoo.com",
-		"address": "Ap #380-1747 Ligula. Road",
-		"postalZip": "88868-880",
-		"currency": "$49.98",
-		"country": "Spain"
-	},
-	{
-		"name": "Dolan Beasley",
-		"phone": "(732) 312-3958",
-		"email": "vivamus@yahoo.edu",
-		"address": "P.O. Box 805, 9831 Amet, Ave",
-		"postalZip": "82677",
-		"currency": "$48.53",
-		"country": "Vietnam"
-	},
-	{
-		"name": "Wanda Castro",
-		"phone": "1-686-582-7660",
-		"email": "in.lorem@aol.net",
-		"address": "Ap #257-4815 Ligula. Street",
-		"postalZip": "P5R 9EY",
-		"currency": "$14.73",
-		"country": "Costa Rica"
-	}
+	// {
+	// 	"name": "Samantha Harper",
+	// 	"phone": "(373) 884-7465",
+	// 	"email": "cras@icloud.com",
+	// 	"address": "145-2621 Tincidunt Road",
+	// 	"postalZip": "U6P 1B3",
+	// 	"currency": "$64.52",
+	// 	"country": "Spain"
+	// },
+	// {
+	// 	"name": "Selma Washington",
+	// 	"phone": "(468) 605-4869",
+	// 	"email": "ac.sem@yahoo.org",
+	// 	"address": "1155 Neque Av.",
+	// 	"postalZip": "1933",
+	// 	"currency": "$38.00",
+	// 	"country": "Mexico"
+	// },
+	// {
+	// 	"name": "Hanna Shaffer",
+	// 	"phone": "(309) 651-2229",
+	// 	"email": "purus.sapien.gravida@hotmail.com",
+	// 	"address": "821-5665 Donec Av.",
+	// 	"postalZip": "724774",
+	// 	"currency": "$52.68",
+	// 	"country": "Netherlands"
+	// },
+	// {
+	// 	"name": "Stewart Finley",
+	// 	"phone": "1-752-721-7198",
+	// 	"email": "vel.faucibus.id@protonmail.ca",
+	// 	"address": "Ap #825-7133 Sed Ave",
+	// 	"postalZip": "2588",
+	// 	"currency": "$84.05",
+	// 	"country": "Colombia"
+	// },
+	// {
+	// 	"name": "Gretchen Logan",
+	// 	"phone": "(669) 298-4624",
+	// 	"email": "aenean.eget.metus@aol.org",
+	// 	"address": "1439 Risus Ave",
+	// 	"postalZip": "96734",
+	// 	"currency": "$43.91",
+	// 	"country": "Vietnam"
+	// },
+	// {
+	// 	"name": "Jasmine Espinoza",
+	// 	"phone": "1-445-947-6981",
+	// 	"email": "malesuada.malesuada.integer@icloud.couk",
+	// 	"address": "7018 Egestas Street",
+	// 	"postalZip": "27774",
+	// 	"currency": "$62.75",
+	// 	"country": "Costa Rica"
+	// },
+	// {
+	// 	"name": "Mona Nash",
+	// 	"phone": "(578) 253-9930",
+	// 	"email": "nec.metus@hotmail.net",
+	// 	"address": "P.O. Box 684, 6990 Odio, Street",
+	// 	"postalZip": "6764",
+	// 	"currency": "$39.14",
+	// 	"country": "United States"
+	// },
+	// {
+	// 	"name": "Roth Burton",
+	// 	"phone": "(995) 628-7064",
+	// 	"email": "sollicitudin.a.malesuada@protonmail.com",
+	// 	"address": "887-6938 Dolor St.",
+	// 	"postalZip": "101674",
+	// 	"currency": "$71.15",
+	// 	"country": "Ireland"
+	// },
+	// {
+	// 	"name": "Hiroko Newton",
+	// 	"phone": "(263) 227-6818",
+	// 	"email": "sed.pede@yahoo.edu",
+	// 	"address": "242-4371 Vehicula St.",
+	// 	"postalZip": "4427",
+	// 	"currency": "$65.40",
+	// 	"country": "Canada"
+	// },
+	// {
+	// 	"name": "Rogan Stanley",
+	// 	"phone": "1-982-267-9189",
+	// 	"email": "sagittis.augue.eu@icloud.ca",
+	// 	"address": "692-6399 Eu, Rd.",
+	// 	"postalZip": "444442",
+	// 	"currency": "$83.36",
+	// 	"country": "Mexico"
+	// },
+	// {
+	// 	"name": "Neil Wall",
+	// 	"phone": "(543) 729-4444",
+	// 	"email": "turpis.nulla@aol.com",
+	// 	"address": "Ap #973-4005 Vitae Ave",
+	// 	"postalZip": "22425-33204",
+	// 	"currency": "$19.14",
+	// 	"country": "Brazil"
+	// },
+	// {
+	// 	"name": "Tanner Cervantes",
+	// 	"phone": "(366) 366-5859",
+	// 	"email": "vel.arcu@yahoo.net",
+	// 	"address": "440-6026 Vel Road",
+	// 	"postalZip": "488404",
+	// 	"currency": "$62.36",
+	// 	"country": "Belgium"
+	// },
+	// {
+	// 	"name": "Merrill Avila",
+	// 	"phone": "1-324-588-3211",
+	// 	"email": "dolor.tempus@protonmail.couk",
+	// 	"address": "135-3622 Libero Ave",
+	// 	"postalZip": "3910",
+	// 	"currency": "$43.99",
+	// 	"country": "United States"
+	// },
+	// {
+	// 	"name": "Alexandra Russo",
+	// 	"phone": "(715) 792-2773",
+	// 	"email": "interdum.enim@google.org",
+	// 	"address": "P.O. Box 954, 1592 Vitae Rd.",
+	// 	"postalZip": "55723",
+	// 	"currency": "$74.02",
+	// 	"country": "Indonesia"
+	// },
+	// {
+	// 	"name": "Tashya Woodward",
+	// 	"phone": "1-991-466-8566",
+	// 	"email": "convallis.dolor.quisque@google.edu",
+	// 	"address": "5133 Vitae Av.",
+	// 	"postalZip": "3823",
+	// 	"currency": "$31.12",
+	// 	"country": "Sweden"
+	// },
+	// {
+	// 	"name": "Merrill Martin",
+	// 	"phone": "1-366-403-5784",
+	// 	"email": "faucibus.leo@yahoo.com",
+	// 	"address": "832-3695 Non Rd.",
+	// 	"postalZip": "K4H 2PN",
+	// 	"currency": "$91.26",
+	// 	"country": "Australia"
+	// },
+	// {
+	// 	"name": "Jeanette Haney",
+	// 	"phone": "(817) 310-8903",
+	// 	"email": "nec.metus.facilisis@yahoo.edu",
+	// 	"address": "638-5626 Lorem Rd.",
+	// 	"postalZip": "35149",
+	// 	"currency": "$85.39",
+	// 	"country": "Sweden"
+	// },
+	// {
+	// 	"name": "Aristotle Gray",
+	// 	"phone": "(274) 377-1991",
+	// 	"email": "augue@hotmail.net",
+	// 	"address": "Ap #711-7355 Velit. Av.",
+	// 	"postalZip": "41205",
+	// 	"currency": "$11.14",
+	// 	"country": "China"
+	// },
+	// {
+	// 	"name": "Jane Stevens",
+	// 	"phone": "(458) 774-6233",
+	// 	"email": "ultrices@hotmail.net",
+	// 	"address": "303 Nunc Rd.",
+	// 	"postalZip": "51539",
+	// 	"currency": "$87.29",
+	// 	"country": "Germany"
+	// },
+	// {
+	// 	"name": "Castor Gibson",
+	// 	"phone": "(645) 385-4416",
+	// 	"email": "quisque.ornare@hotmail.ca",
+	// 	"address": "220-7949 Nunc Rd.",
+	// 	"postalZip": "2822",
+	// 	"currency": "$43.45",
+	// 	"country": "Spain"
+	// },
+	// {
+	// 	"name": "Melyssa Castillo",
+	// 	"phone": "(286) 146-6744",
+	// 	"email": "quam@google.ca",
+	// 	"address": "Ap #971-1948 Montes, Road",
+	// 	"postalZip": "122738",
+	// 	"currency": "$50.50",
+	// 	"country": "Costa Rica"
+	// },
+	// {
+	// 	"name": "Julie Ingram",
+	// 	"phone": "(472) 323-6759",
+	// 	"email": "amet.massa@yahoo.com",
+	// 	"address": "Ap #380-1747 Ligula. Road",
+	// 	"postalZip": "88868-880",
+	// 	"currency": "$49.98",
+	// 	"country": "Spain"
+	// },
+	// {
+	// 	"name": "Dolan Beasley",
+	// 	"phone": "(732) 312-3958",
+	// 	"email": "vivamus@yahoo.edu",
+	// 	"address": "P.O. Box 805, 9831 Amet, Ave",
+	// 	"postalZip": "82677",
+	// 	"currency": "$48.53",
+	// 	"country": "Vietnam"
+	// },
+	// {
+	// 	"name": "Wanda Castro",
+	// 	"phone": "1-686-582-7660",
+	// 	"email": "in.lorem@aol.net",
+	// 	"address": "Ap #257-4815 Ligula. Street",
+	// 	"postalZip": "P5R 9EY",
+	// 	"currency": "$14.73",
+	// 	"country": "Costa Rica"
+	// }
 ];
