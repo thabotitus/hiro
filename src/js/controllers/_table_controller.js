eb736203ed54991f9fd67628a16e40a65f8de421
Thabo <thabotitus@gmail.com>
2021-12-07 21:05:04 +0200

    cleanup

--- src/js/controllers/_table_controller.js
+++ src/js/controllers/_table_controller.js
@@ -5,21 +5,7 @@ export default class extends Controller {
 
   SELECTED_ROW_CLASS = 'hi-table__tr--selected';
 
-  connect() {
-    this.initSelectionRowCount();
-  }
-
-  selectRow(e) {
-    e.target.closest('tr').classList.toggle(this.SELECTED_ROW_CLASS);
-    this.updateSelectedCount();
-  }
-
-  initSelectionRowCount() {
-    this.selectedRowCountTarget.innerHTML = '<b>0</b> rows selected';
-  }
-
-  updateSelectedCount() {
-    const count = document.getElementsByClassName(this.SELECTED_ROW_CLASS).length;
-    this.selectedRowCountTarget.innerHTML = `<b>${count}</b> rows selected`;
+  selectRow(event) {
+    event.target.closest('tr').classList.toggle(this.SELECTED_ROW_CLASS);
   }
 }
