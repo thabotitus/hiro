3a4cb3c5f9f96aaf0e310b203c85a1792940b89c
Thabo <thabotitus@gmail.com>
2021-12-14 22:01:31 +0200

    show active parent

--- src/js/controllers/_navigation_controller.ts
+++ src/js/controllers/_navigation_controller.ts
@@ -8,7 +8,7 @@ export default class extends Controller {
   offcanvasTargets:      HTMLElement[];
   offcanvasTarget:       HTMLElement;
   hasOffcanvasTarget:    boolean;
-
+  
   static targets: string[] = ["navigation", "offcanvas"];
 
   connect = (): void => {
@@ -26,6 +26,16 @@ export default class extends Controller {
   }
 
   setActiveNavigationItems = (): void => {
-    
+    const currentUrl: string = window.location.href;
+    const navItems: NodeListOf<HTMLElement> = document.querySelectorAll('[data-url]');
+
+    const activeItem: HTMLElement = [...navItems].find((el: HTMLElement): boolean => {
+      return currentUrl.match(el.dataset.url) !== null;
+    });
+
+    const parentId: string = activeItem.parentElement.id;
+    const toggleableParents: NodeListOf<HTMLElement> = document.querySelectorAll(`a[href='#${parentId}']`);
+
+    [...toggleableParents].forEach((item: HTMLElement) => item.click());
   }
 }
